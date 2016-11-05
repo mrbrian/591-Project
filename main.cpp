17b31377adebcd512b456aef1c85898f873dd370
Brian Yee <Brian Yee>
2016-11-05 13:20:45 -0600

    Cornell scene done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <iostream>
 
-#define DEF_WIDTH   200
-#define DEF_HEIGHT  200
+#define DEF_WIDTH   250
+#define DEF_HEIGHT  250
 
 double clamp(double min, double max, double in)
 {
@@ -16,30 +16,35 @@ double clamp(double min, double max, double in)
     return in;
 }
 
-// source: http://www.graphics.cornell.edu/online/box/data.html
+double degToRad(float deg)
+{
+	return (M_PI / 180 * deg);
+}
+
+// cornell scene
 void SetupCornellBox(Scene *s, int width, int height)
 {
-    Scene &scene = *s;
-    Camera cam = Camera();
+	Scene &scene = *s;
+	Camera cam = Camera();
 
-    cam.imgWidth = width;
-    cam.imgHeight = height;
-    cam.lookAt = Point3D(0, 0, -1);
-    cam.position = Point3D(0, 0, 0);
+	cam.imgWidth = width;
+	cam.imgHeight = height;
+	cam.lookAt = Point3D(0, 0, -1);
+	cam.position = Point3D(0, 0, 0);
 
-    scene.cam = cam;
+	scene.cam = cam;
 	float img_plane_w = 0.5f;
-    scene.imgPlane = new Plane(Point3D(-img_plane_w, img_plane_w, -1), Point3D(-img_plane_w,-img_plane_w, -1), Point3D(img_plane_w,-img_plane_w, -1), Point3D(img_plane_w, img_plane_w, -1));
+	scene.imgPlane = new Plane(Point3D(-img_plane_w, img_plane_w, -1), Point3D(-img_plane_w, -img_plane_w, -1), Point3D(img_plane_w, -img_plane_w, -1), Point3D(img_plane_w, img_plane_w, -1));
 
-    Material *mat_ceil = new Material(Colour(0,0,0), Colour(1, 1, 1), Colour(1,1,1), 1000, Colour(0, 0, 0));
-    Material *mat_grn = new Material(Colour(0,0,0), Colour(0, 0.5f, 0), Colour(1,1,1), 100, Colour(0, 0, 0));
+	Material *mat_ceil = new Material(Colour(0, 0, 0), Colour(1, 1, 1), Colour(1, 1, 1), 1000, Colour(0, 0, 0));
+	Material *mat_grn = new Material(Colour(0, 0, 0), Colour(0, 0.5f, 0), Colour(1, 1, 1), 100, Colour(0, 0, 0));
 	Material *mat_red = new Material(Colour(0, 0, 0), Colour(0.5f, 0, 0), Colour(1, 1, 1), 10, Colour(0, 0, 0));
 	Material *mat_floor = new Material(Colour(0, 0, 0), Colour(0.6f, 0.6f, 0.6f), Colour(1, 1, 1), 10, Colour(0, 0, 0));
 
-    Light *light = new Light(Point3D(0, 2.65, -8), Colour(0.1,0.1,0.1), Colour(0.5,0.5,0.5), Colour(0,0,0));
-    scene.lights.push_back(light);
+	Light *light = new Light(Point3D(0, 2.65, -8), Colour(0.1, 0.1, 0.1), Colour(0.5, 0.5, 0.5), Colour(0, 0, 0));
+	scene.lights.push_back(light);
 
-    // Ceiling
+	// Ceiling
 	Quad *q_1 = new Quad(
 		Point3D(2.75, 2.75, -10.5),
 		Point3D(2.75, 2.75, -5),
@@ -48,6 +53,15 @@ void SetupCornellBox(Scene *s, int width, int height)
 		mat_ceil);
 
 	scene.objects.push_back(q_1);
+
+	// Ceiling light
+	Quad *light_q = new Quad(
+		Point3D(0.653, 2.74, -8.274),
+		Point3D(-0.653, 2.74, -8.274),
+		Point3D(-0.653, 2.74, -7.224),
+		Point3D(0.653, 2.74, -7.224),
+		mat_red);
+	scene.objects.push_back(light_q);
 
 	// Green wall on right
 	Quad *q_2 = new Quad(
@@ -58,7 +72,7 @@ void SetupCornellBox(Scene *s, int width, int height)
 		mat_grn);
 	scene.objects.push_back(q_2);
 
- //   // Red wall on left
+	//   // Red wall on left
 	Quad *q_3 = new Quad(
 		Point3D(2.75, 2.75, -10.5),
 		Point3D(2.75, -2.75, -10.5),
@@ -67,7 +81,7 @@ void SetupCornellBox(Scene *s, int width, int height)
 		mat_red);
 	scene.objects.push_back(q_3);
 
- //   // Floor
+	//   // Floor
 	Quad *q_4 = new Quad(
 		Point3D(2.75, -2.75, -10.5),
 		Point3D(-2.75, -2.75, -10.5),
@@ -76,7 +90,7 @@ void SetupCornellBox(Scene *s, int width, int height)
 		mat_floor);
 	scene.objects.push_back(q_4);
 
-    // Back wall
+	// Back wall
 	Quad *q_5 = new Quad(
 		Point3D(2.75, 2.75, -10.5),
 		Point3D(-2.75, 2.75, -10.5),
@@ -84,6 +98,30 @@ void SetupCornellBox(Scene *s, int width, int height)
 		Point3D(2.75, -2.75, -10.5),
 		mat_floor);
 	scene.objects.push_back(q_5);
+
+	Cube *sml_cube = new Cube(
+		Point3D(0, 0, 0),
+		0.5,
+		mat_ceil
+	);
+	sml_cube->Transform(
+		Matrix4x4::translation(Vector3D(-0.929, -2.75 + 3.31 / 2, -8.482)) *
+		Matrix4x4::rotation(degToRad(-18.809), 'y') *
+		Matrix4x4::scaling(Vector3D(1.659, 3.31, 1.659))
+	);
+	scene.objects.push_back(sml_cube);
+
+	Cube *big_cube = new Cube(
+		Point3D(0,0,0),
+		0.5,
+		mat_ceil
+	);
+	big_cube->Transform(
+		Matrix4x4::translation(Vector3D(0.933, -2.75 + 1.655 / 2, -6.648)) *
+		Matrix4x4::rotation(degToRad(16.303), 'y') *
+		Matrix4x4::scaling(Vector3D(1.655, 1.655, 1.655)) 
+	);
+	scene.objects.push_back(big_cube);
 }
 
 int main(int argc, char *argv[])
