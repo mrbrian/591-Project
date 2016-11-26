f96a641ee8c60e59951e131c21ea93750f1a3fb2
Brian Yee <bgyee@ucalgary.ca>
2016-11-25 21:20:52 -0800

    trying to debug imageplane

--- tests.cpp
+++ tests.cpp
@@ -130,7 +130,105 @@ void proj_point()
 
 void proj_point2()
 {
+    vector<Point3D> photon_pos;
+
+
     printf("proj_point2\n");
+    Matrix4x4 m;
+    m = m.rotation(M_PI, 'y');
+    vector<photon*> p_list;
+    {
+        photon *p = new photon();
+        Point3D p_p = Point3D(1,0,1);
+        p_p = m * p_p;
+        p->set_position(p_p);
+        p->set_direction(Vector3D(0,0,1));
+        p->set_color(Color(1,0,1));
+        p_list.push_back(p);
+    }
+    photon *p2 = new photon();
+    Point3D p_p2 = Point3D(0,1,1);
+    p_p2 = m * p_p2;
+    p2->set_position(p_p2);
+    p2->set_direction(Vector3D(0,0,1));
+    p2->set_color(Color(0,1,1));
+    p_list.push_back(p2);
+
+    photon *p3 = new photon();
+    Point3D p_p3 = Point3D(1,1,1);
+    p_p3 = m * p_p3;
+    p3->set_position(p_p3);
+    p3->set_direction(Vector3D(0,0,1));
+    p3->set_color(Color(0,1,0));
+    p_list.push_back(p3);
+
+    {
+        photon *p = new photon();
+        Point3D p_p = Point3D(-1,-1,1);
+        p_p = m * p_p;
+        p->set_position(p_p);
+        p->set_direction(Vector3D(1,0,0));
+        p->set_color(Color(1,1,0));
+        p_list.push_back(p);
+    }
+
+    Scene s;
+    s.cam.position = Point3D(0,0,0);
+
+    //float aspect = (float)width() / height();
+
+    s.cam.fov = (float)DEF_VIEW_FOV / 180 * M_PI;
+    s.cam.far = DEF_VIEW_FAR;
+    s.cam.near = DEF_VIEW_NEAR;
+    s.cam.aspect = 1;
+    s.cam.imgHeight = 5;
+    s.cam.imgWidth = 5;
+
+    s.cam.m_view = m;
+
+    float img_plane_w = 0.5f;
+    s.imgPlane = s.cam.calc_img_plane();
+    for (int y = 0; y < 4; y++)
+    {
+        printf("%f %f %f\n", s.imgPlane->points[y][0], s.imgPlane->points[y][1], s.imgPlane->points[y][2], s.imgPlane->points[y][3]);
+    }
+
+    int width = s.cam.imgWidth;
+    int height = s.cam.imgHeight;
+
+    std::vector<unsigned char> img;
+    img.resize(width * height * 4);
+
+    Color *image = s.Render(&p_list);
+    for (int y = 0; y < s.cam.imgHeight; y++)
+    {
+        for (int x = 0; x < s.cam.imgWidth; x++)
+        {
+            printf("%d,%d - %f %f %f\n", x,y,image[y*s.cam.imgWidth + x].R(), image[y*s.cam.imgWidth + x].G(), image[y*s.cam.imgWidth + x].B());
+        }
+    }
+
+    for (int x = 0; x < s.cam.imgWidth; x++)
+    {
+        for (int y = 0; y < s.cam.imgHeight; y++)
+        {
+            int idx = x + y * s.cam.imgWidth;
+            Color &c = image[idx];
+            // clamp rgb values [0,1]
+            img[4 * idx + 0] = clamp(0, 1, c.R()) * 255;
+            img[4 * idx + 1] = clamp(0, 1, c.G()) * 255;
+            img[4 * idx + 2] = clamp(0, 1, c.B()) * 255;
+            img[4 * idx + 3] = 255;
+        }
+    }
+    // save to file
+    lodepng::encode("proj_point2", img, s.cam.imgWidth, s.cam.imgHeight);
+
+}
+
+void camera_plane()
+{
+    printf("camera_plane\n");
     Scene s;
     s.cam.position = Point3D(0,0,0);
 
@@ -144,15 +242,42 @@ void proj_point2()
     s.cam.imgWidth= 5;
 
     Matrix4x4 m;
+
+    s.imgPlane = s.cam.calc_img_plane();
+
+    for (int y = 0; y < 4; y++)
+    {
+        printf("%f %f %f\n", s.imgPlane->points[y][0], s.imgPlane->points[y][1], s.imgPlane->points[y][2], s.imgPlane->points[y][3]);
+    }
+
     m = m.translation(Vector3D(0,0,0));
     m = m.rotation(M_PI, 'y');
     s.cam.m_view = m;
 
-    float img_plane_w = 0.5f;
+    Vector3D expected[4] = {
+        Vector3D(0,0,0),
+        Vector3D(0,0,0),
+        Vector3D(0,0,0),
+        Vector3D(0,0,0)
+    };
+
+    printf("camera_planeb\n");
+
     s.imgPlane = s.cam.calc_img_plane();
+
     for (int y = 0; y < 4; y++)
     {
-        printf("%f %f %f\n", s.imgPlane->points[y][0], s.imgPlane->points[y][1], s.imgPlane->points[y][2], s.imgPlane->points[y][3]);
+        printf("%f %f %f\n", s.imgPlane->points[y][0], s.imgPlane->points[y][1], s.imgPlane->points[y][2]);
+    }
+
+    for (int x = 0; x < s.cam.imgWidth; x++)
+    {
+        for (int y = 0; y < s.cam.imgHeight; y++)
+        {
+            // determine ray vector
+            Point3D p = s.imgPlane->ImageToWorldSpace(x, y, s.cam.imgWidth, s.cam.imgHeight);
+            printf("%f %f %f\n", p[0], p[1], p[2]);
+        }
     }
 }
 
@@ -161,6 +286,7 @@ tests::tests()
     sphericalCoord_1();
     proj_point();
     proj_point2();
+    camera_plane();
 }
 
 int main(int argc, char *argv[])
